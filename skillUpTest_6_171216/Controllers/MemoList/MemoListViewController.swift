59260cc4f9998583eafcb59d67ce6a1e559ad48a
S N <will@kpc-014c1104-3.local>
2017-12-27 16:10:31 +0900

    Added. ストーリーボードとの繋がり

--- skillUpTest_6_171216/Controllers/MemoList/MemoListViewController.swift
+++ skillUpTest_6_171216/Controllers/MemoList/MemoListViewController.swift
@@ -9,9 +9,14 @@
 import UIKit
 
 class MemoListViewController: UIViewController {
-
+    @IBOutlet weak var leftBarButtonItem: UIBarButtonItem!
+    @IBOutlet weak var rightBarButtonItem: UIBarButtonItem!
+    @IBOutlet weak var memoListTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        navigationItem.rightBarButtonItem = editButtonItem
 
         // Do any additional setup after loading the view.
     }
@@ -20,5 +25,16 @@ class MemoListViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    override func setEditing(_ editing: Bool, animated: Bool) {
+        super.setEditing(editing, animated: animated)
+        memoListTableView.isEditing = !memoListTableView.isEditing
+        if(memoListTableView.isEditing){
+            leftBarButtonItem.title = "すべて削除"
+        }else{
+            leftBarButtonItem.title = "メモ追加"
+        }
+    }
+    @IBAction func tappedLeftBarButtonItem(_ sender: UIBarButtonItem) {
+    }
+    
 }
