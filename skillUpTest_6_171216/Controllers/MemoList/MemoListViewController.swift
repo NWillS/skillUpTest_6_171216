e0d453aacd175b02d35070ebcb9c7a39ee56758e
S N <will@kpc-014c1104-3.local>
2017-12-27 15:59:53 +0900

    Added. MemoList

---
+++ skillUpTest_6_171216/Controllers/MemoList/MemoListViewController.swift
@@ -0,0 +1,24 @@
+//
+//  MemoListViewController.swift
+//  skillUpTest1216fin
+//
+//  Created by S N on 2017/12/27.
+//  Copyright © 2017年 Seidi Nakamura. All rights reserved.
+//
+
+import UIKit
+
+class MemoListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
